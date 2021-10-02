2623b48a47daf09b4bd659b0dd088a09816864db
Deepakbiocrafe <deepakvijayan.mail@gmail.com>
2021-10-02 16:00:21 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Red Rose',
+            category:'Rose',
+            image:'/images/Roseimg.jpg',
+            price:150,
+            color: 'red',
+            rating: 4.5,
+            numReviews:4,
+            description:'Colorful roses'
+        },
+        {
+            _id:'2',
+            name:'White Tulips',
+            category:'Tulip',
+            image:'/images/Tulipimg.jpg',
+            price:160,
+            color: 'white',
+            rating: 4,
+            numReviews:3,
+            description:'Colorful Tulips'
+        },
+        {
+            _id:'3',
+            name:'White Lilly',
+            category:'Lilly',
+            image:'/images/Lillyimg.jpg',
+            price:120,
+            color: 'white',
+            rating: 3.5,
+            numReviews:6,
+            description:'White Lillies'
+        },
+        {
+            _id:'4',
+            name:'Daffodil Flower',
+            category:'Daffodil',
+            image:'/images/Dafodilimg.jpg',
+            price:250,
+            color: 'yellow',
+            rating: 4.5,
+            numReviews:2,
+            description:'Yellow Daffodil'
+        },
+        {
+            _id:'5',
+            name:'Orchid flower',
+            category:'Orchid',
+            image:'/images/orchidimg.jpg',
+            price:180,
+            color: 'Violet',
+            rating: 4,
+            numReviews:6,
+            description:'Violet orchid'
+        },
+        {
+            _id:'6',
+            name:'White Jasmine',
+            category:'Jasmine',
+            image:'/images/jasmineimg.jpg',
+            price:200,
+            color: 'White',
+            rating: 2.5,
+            numReviews:8,
+            description:'White jasmine'
+        },
+    ],
+};
+
+export default data;
